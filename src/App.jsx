d24afdbb15b10a8765e9a203db55efe38e3fd29c
Felix Ewulu <cmekusfelix8@gmail.com>
2023-09-01 20:51:37 +0100

    completed the header component and set currentUser

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { accounts } from "./accounts";
+import Header from "./components/header/Header";
 
 const App = () => {
-	return <div>App</div>;
+	const [newAccounts, setNewAccounts] = useState(accounts);
+	const [currentUser, setCurrentUser] = useState(null);
+
+	// sets a currentuser when a user logs in
+	const handleLogin = (user) => {
+		setCurrentUser(user);
+	};
+
+	return (
+		<>
+			<Header
+				accounts={accounts}
+				onLogin={handleLogin}
+				currentUser={currentUser}
+			/>
+		</>
+	);
 };
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Header = ({ accounts, onLogin, currentUser }) => {
+	const [username, setUsername] = useState("");
+	const [pin, setPin] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (!username || !pin) return;
+
+		const currentAccount = accounts?.find(
+			(acc) => acc.owner === username && acc.pin === pin
+		);
+
+		onLogin(currentAccount);
+	};
+
+	return (
+		<nav>
+			<p className="welcome">
+				{currentUser
+					? `Welcome back ${currentUser?.owner.split(" ")[0]}`
+					: "Log in to get started"}
+			</p>
+
+			<img src="/logo.png" alt="logo" className="logo" />
+
+			<form className="login" onSubmit={handleSubmit}>
+				<input
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					type="text"
+					placeholder="user"
+					className="login__input login__input--user"
+				/>
+
+				<input
+					value={pin}
+					onChange={(e) => setPin(Number(e.target.value))}
+					type="password"
+					placeholder="PIN"
+					maxLength="4"
+					className="login__input login__input--pin"
+				/>
+				<button className="login__btn">&rarr;</button>
+			</form>
+		</nav>
+	);
+};
+
+export default Header;
