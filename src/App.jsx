1d267fbb32b4ac01e552a5e33df418f0c9a96882
Felix Ewulu <cmekusfelix8@gmail.com>
2023-09-01 20:26:27 +0100

    added all needed component folders

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { accounts } from "./accounts";
+
+const App = () => {
+	return <div>App</div>;
+};
+
+export default App;
---
+++ src/accounts.js
@@ -0,0 +1,26 @@
+export const accounts = [
+	{
+		owner: "Felix",
+		movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+		interestRate: 1.2,
+		pin: 1111,
+	},
+	{
+		owner: "Steve",
+		movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+		interestRate: 1.5,
+		pin: 2222,
+	},
+	{
+		owner: "Justin",
+		movements: [200, -200, 340, -300, -20, 50, 400, -460],
+		interestRate: 0.7,
+		pin: 3333,
+	},
+	{
+		owner: "Jonas",
+		movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+		interestRate: 1.2, // %
+		pin: 4444,
+	},
+];
